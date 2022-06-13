625a6be41bcb79a5a2d04c83b34da465217cb0ba
ElAskal <aviryu17000@hotmail.fr>
2022-06-13 14:51:51 +0200

    Commit '

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ document.getElementById('submit').onclick = function () {
     var selected = document.querySelector('input[type=radio]:checked');
     console.log(selected.value);
     document.getElementById('finalRating').innerHTML = selected.value;
+    document.getElementById('rating').style.visibility = "hidden";
+    document.getElementById('thanks').style.visibility = "visible";
 };
